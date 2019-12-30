ed2d875d2ea938005b166da247ff522fc05e28d8
Slimshady <55093243+GuoYiyang@users.noreply.github.com>
2019-12-30 19:00:59 +0800

    Update

--- mian.py
+++ mian.py
@@ -6,7 +6,10 @@ options = webdriver.ChromeOptions()
 options.add_argument(
     'user-agent="Mozilla/5.0 (Linux; Android 4.0.4; Galaxy Nexus Build/IMM76B) AppleWebKit/535.19 (KHTML, like Gecko) Chrome/18.0.1025.133 Mobile Safari/535.19"'
 )
+select_net = 0
 
+    
+#登录
 def login():
     driver = webdriver.Chrome(chrome_options=options)
     driver.get(url='http://10.2.5.251/')
@@ -35,7 +38,7 @@ def login():
     denglu = driver.find_element_by_xpath(
         '//*[@id="edit_body"]/div[2]/div/form/input[1]')
     denglu.click()
-
+#注销
 def logout():
     driver = webdriver.Chrome(chrome_options=options)
     driver.get(url='http://10.2.5.251/')
@@ -49,24 +52,30 @@ def handler(event):
     select_net = current
 
 
+#图形界面
 root = Tk()
 root.title('CUMT_NET')
+#标签
 Label(root,text="账号:").grid(row=0,column=0,padx=10,pady=5)
 Label(root,text="密码:").grid(row=1,column=0,padx=10,pady=5)
 Label(root,text="运营商:").grid(row=2,column=0,padx=10,pady=5)
-
-e1 = Entry(root)
-e2 = Entry(root,show="*")
+#输入
+default_xuehao = StringVar(value='05171753')
+default_mima = StringVar(value='200711')
+e1 = Entry(root,textvariable=default_xuehao)
+e2 = Entry(root,textvariable=default_mima,show="*")
 e1.grid(row=0,column=1,padx=10,pady=5)
 e2.grid(row=1,column=1,padx=10,pady=5)
-
-select_net = 0
+#选择框
 values=['校园网','中国移动','中国联通','中国电信']
 combobox = ttk.Combobox(root, values=values)
 combobox.bind('<<ComboboxSelected>>', handler)
 combobox.grid(row=2,column=1,padx=10,pady=5)
-
+#按钮
 Button(root,text="登录",command=login).grid(row=3,column=0,sticky="w",padx=10,pady=5)
 Button(root,text="注销",command=logout).grid(row=3,column=1,sticky="e",padx=10,pady=5)
 
 mainloop()
+
+if __name__ == "__main__":
+    pass
